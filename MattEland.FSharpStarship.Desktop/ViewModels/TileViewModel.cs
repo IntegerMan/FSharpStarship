b1d284afe0a147827696e1e7444d1d3512781165
Matt Eland <Matt.Eland@GMail.com>
2020-05-31 22:32:53 -0400

    Custom coloration is now available

--- MattEland.FSharpStarship.Desktop/ViewModels/TileViewModel.cs
+++ MattEland.FSharpStarship.Desktop/ViewModels/TileViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MattEland.FSharpStarship.Logic;
+﻿using System.Windows.Media;
+using MattEland.FSharpStarship.Logic;
 
 namespace MattEland.FSharpStarship.Desktop.ViewModels
 {
@@ -18,5 +19,19 @@ namespace MattEland.FSharpStarship.Desktop.ViewModels
 
         public int PosX => Tile.pos.x * TileWidth;
         public int PosY => Tile.pos.y * TileHeight;
+
+        public Brush Background
+        {
+            get
+            {
+                var color = Common.getBackgroundColor(Tile);
+
+                // TODO: Introduce a brush factory to store unique / reused brushes
+                Brush brush = new SolidColorBrush(Color.FromRgb(color.r, color.g, color.b));
+                brush.Freeze();
+
+                return brush;
+            }
+        }
     }
 }
--- MattEland.FSharpStarship.Logic/Library.fs
+++ MattEland.FSharpStarship.Logic/Library.fs
@@ -29,6 +29,14 @@ module Common =
 
   type Tile = {tileType: TileType; pos: Pos}
 
+  type RGB = {r: byte; g: byte; b: byte}
+
+  let getBackgroundColor (tile: Tile): RGB =
+    match tile.tileType with
+    | Floor -> {r=86uy; g=86uy; b=128uy}
+    | Wall -> {r=64uy; g=64uy; b=84uy}
+    | _ -> {r=255uy; g=0uy; b=255uy}
+
   let makeWall pos = {tileType=Wall; pos=pos}
   let makeFloor pos = {tileType=Floor; pos=pos}
 
