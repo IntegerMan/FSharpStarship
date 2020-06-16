c7dac147be9db25195260b0bb55298c20aa9a04f
Matt Eland <Matt.Eland@GMail.com>
2020-06-16 01:02:30 -0400

    Added Hospital16 tileset

---
+++ MattEland.FSharpStarship.Desktop/Hospital48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Hospital48" tilewidth="48" tileheight="48" tilecount="464" columns="16">
+ <image source="Images/Hospital.png" width="768" height="1392"/>
+</tileset>
---
+++ MattEland.FSharpStarship.Desktop/ScifiHospitalTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="ScifiHospitalTiles" tilewidth="32" tileheight="32" tilecount="136" columns="8">
+ <image source="Images/tileset2.png" width="256" height="544"/>
+</tileset>
