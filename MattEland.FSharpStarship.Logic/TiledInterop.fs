645e0b9f65fdb6217454d038c62e9f5d6f6ddcf7
Matt Eland <Matt.Eland@GMail.com>
2020-06-18 00:26:05 -0400

    Door layer loading
    I'm going to need to refactor my entire rendering layer to take advantage of Tiled here...

--- MattEland.FSharpStarship.Logic/TiledInterop.fs
+++ MattEland.FSharpStarship.Logic/TiledInterop.fs
@@ -13,18 +13,47 @@ module TiledInterop =
 
   let getObjectPos (o: TiledSharp.TmxObject) = pos (pixelToTile o.X) (pixelToTile o.Y)
 
+  let allTiledObjects (tilemap: TiledSharp.TmxMap) = tilemap.ObjectGroups |> Seq.collect(fun g -> g.Objects)
+
+  let objectsOfType typeName tilemap = 
+    tilemap
+    |> allTiledObjects
+    |> Seq.where(fun o -> o.Type = typeName)
+
+  let mapObjectTypeByFunc typeName createFunc (tilemap: TiledSharp.TmxMap) =
+    tilemap
+    |> objectsOfType typeName
+    |> Seq.map(createFunc)
+
+  let mapObjectType typeName objectType (tilemap: TiledSharp.TmxMap) =
+    tilemap
+    |> mapObjectTypeByFunc typeName (fun obj -> {ObjectType=objectType; Pos=(getObjectPos obj)})
+
+  let getTilesFromLayer (layerName: string) (tilemap: TiledSharp.TmxMap) = 
+    let layer = tilemap.Layers.[layerName]
+    layer.Tiles |> Seq.filter(fun t -> t.Gid > 0)
+    
   let getObjects (tilemap: TiledSharp.TmxMap): List<GameObject> =
-    tilemap.ObjectGroups
-    |> Seq.collect(fun g -> g.Objects)
-    |> Seq.where(fun o -> o.Type = "Astronaut")
-    |> Seq.map(fun a -> {ObjectType=Astronaut; Pos=(getObjectPos a)})
-    |> Seq.toList
+    let astronauts = tilemap |> mapObjectType "Astronaut" Astronaut
+    let doors = 
+      tilemap 
+      |> getTilesFromLayer "Doors" 
+      |> Seq.map(fun d -> {ObjectType=Door(IsOpen=false, IsHorizontal=false); Pos=getTilePos d})
+
+    Seq.append astronauts doors |> Seq.toList
 
   let getTiles (tilemap: TiledSharp.TmxMap): List<Tile> =
-    tilemap.Layers.["Floor"].Tiles
-    |> Seq.filter(fun t -> t.Gid > 0)
-    |> Seq.map(fun t -> makeTile TileType.Floor (getTilePos t))
-    |> Seq.toList
+    let floorTiles = 
+      tilemap
+      |> getTilesFromLayer "Floor"
+      |> Seq.map(fun t -> makeTile TileType.Floor (getTilePos t))
+
+    let wallTiles = 
+      tilemap
+      |> getTilesFromLayer "Walls"
+      |> Seq.map(fun t -> makeTile TileType.Wall (getTilePos t))
+    
+    Seq.append floorTiles wallTiles |> Seq.toList
 
   let loadWorld (filename: string) =
     let tiledFile = new TiledSharp.TmxMap(filename)
