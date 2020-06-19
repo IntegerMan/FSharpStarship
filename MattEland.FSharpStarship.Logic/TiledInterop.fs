6f8ccac2986ffeb9fb8d834fef58da1a461726da
Matt Eland <Matt.Eland@GMail.com>
2020-06-18 23:24:00 -0400

    More mature mapping

--- MattEland.FSharpStarship.Logic/TiledInterop.fs
+++ MattEland.FSharpStarship.Logic/TiledInterop.fs
@@ -4,32 +4,41 @@ open Positions
 open World
 open Tiles
 open GameObjects
+open TiledSharp
 
 module TiledInterop =
 
-  let getTilePos (t: TiledSharp.TmxLayerTile) = pos t.X t.Y
+  type TiledInfo = 
+    {
+      Floor: seq<TmxLayerTile>
+      Walls: seq<TmxLayerTile>
+      Doors: seq<TmxLayerTile>
+      Objects: seq<TmxObject>
+    }
+
+  let getTilePos (t: TmxLayerTile) = pos t.X t.Y
 
   let pixelToTile (v: float): int = (int v) / 32
 
-  let getObjectPos (o: TiledSharp.TmxObject) = pos (pixelToTile o.X) (pixelToTile o.Y)
+  let getObjectPos (o: TmxObject) = pos (pixelToTile o.X) (pixelToTile o.Y)
 
-  let allTiledObjects (tilemap: TiledSharp.TmxMap) = tilemap.ObjectGroups |> Seq.collect(fun g -> g.Objects)
+  let allTiledObjects (tilemap: TmxMap) = tilemap.ObjectGroups |> Seq.collect(fun g -> g.Objects)
 
   let objectsOfType typeName tilemap = 
     tilemap
     |> allTiledObjects
     |> Seq.where(fun o -> o.Type = typeName)
 
-  let mapObjectTypeByFunc typeName createFunc (tilemap: TiledSharp.TmxMap) =
+  let mapObjectTypeByFunc typeName createFunc (tilemap: TmxMap) =
     tilemap
     |> objectsOfType typeName
     |> Seq.map(createFunc)
 
-  let mapObjectType typeName objectType (tilemap: TiledSharp.TmxMap) =
+  let mapObjectType typeName objectType (tilemap: TmxMap) =
     tilemap
     |> mapObjectTypeByFunc typeName (fun obj -> {ObjectType=objectType; Pos=(getObjectPos obj)})
 
-  let getTilesFromLayer (layerName: string) (tilemap: TiledSharp.TmxMap) = 
+  let getTilesFromLayer (layerName: string) (tilemap: TmxMap) = 
     let layer = tilemap.Layers.[layerName]
     layer.Tiles |> Seq.filter(fun t -> t.Gid > 0)
     
@@ -55,8 +64,60 @@ module TiledInterop =
     
     Seq.append floorTiles wallTiles |> Seq.toList
 
+  let interpretWorld tilemap =
+    {
+      Floor = tilemap |> getTilesFromLayer "Floor"
+      Walls = tilemap |> getTilesFromLayer "Walls"
+      Doors = tilemap |> getTilesFromLayer "Doors"
+      Objects = tilemap |> allTiledObjects
+    }
+
+  let translateToTile tileType tmxTile =
+    tmxTile
+    |> getTilePos
+    |> makeTile tileType
+
+  let translateToObject (tmxObject: TmxObject) = 
+    let objectType =
+      match tmxObject.Type with
+      | "Astronaut" -> Astronaut
+    {ObjectType=objectType; Pos=getObjectPos tmxObject}
+
+  let mergeWith (nextLayer: seq<Tile>) (baseLayer: seq<Tile>) = 
+    baseLayer
+    |> Seq.filter(fun st -> nextLayer |> Seq.tryFind(fun nt -> nt.Pos = st.Pos) |> Option.isNone)
+    |> Seq.append nextLayer
+
+  let createDoor (walls: seq<Tile>) doorTile =
+    let pos = doorTile |> getTilePos
+
+    let isHorizontal = 
+      walls
+      |> Seq.tryFind(fun w -> w.Pos = (pos |> offset 0 -1))
+      |> Option.isSome
+
+    {ObjectType=Door(IsOpen=false, IsHorizontal=isHorizontal); Pos=pos}
+
+  let generateWorld data =
+    let floors = data.Floor |> Seq.map(fun t -> t |> translateToTile Floor)
+    let walls = data.Walls |> Seq.map(fun t -> t |> translateToTile Wall)
+    let doors = data.Doors |> Seq.map(fun d -> d |> createDoor walls)
+
+    let tiles = 
+      floors 
+      |> mergeWith walls 
+      |> Seq.toList
+
+    let objects = 
+      data.Objects 
+      |> Seq.map(translateToObject) 
+      |> Seq.append doors
+      |> Seq.toList
+
+    create tiles objects
+
   let loadWorld (filename: string) =
     let tiledFile = new TiledSharp.TmxMap(filename)
-    let tiles = tiledFile |> getTiles
-    let objects = tiledFile |> getObjects
-    create tiles objects
+    tiledFile 
+    |> interpretWorld
+    |> generateWorld
